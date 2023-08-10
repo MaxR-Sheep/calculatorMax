6776bfc1b29147c822f085d7c8966be6833509a4
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-08-10 15:02:27 +0200

    ajout affichage 1 et 2 avant ajout touche égale

--- calculator.js
+++ calculator.js
@@ -5,8 +5,16 @@ keys.addEventListener("click", (event) => {
   if (event.target.matches("button")) {
     const key = event.target;
     const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
+
     if (!action) {
-      console.log("number key!");
+      if (displayedNum === "0" || previousKeyType === "operator") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
     }
     if (
       action === "add" ||
@@ -14,18 +22,42 @@ keys.addEventListener("click", (event) => {
       action === "multiply" ||
       action === "divide"
     ) {
-      console.log("operator key!");
+      calculator.dataset.previousKeyType = "operator";
     }
+    Array.from(key.parentNode.children).forEach((k) =>
+      k.classList.remove("is-depressed")
+    );
     if (action === "decimal") {
-      console.log("decimal key!");
+      display.textContent = displayedNum + ".";
     }
 
     if (action === "clear") {
-      console.log("clear key!");
+      calculator.dataset.previousKeyType = "clear";
     }
 
     if (action === "calculate") {
-      console.log("equal key!");
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
+      const calculate = (n1, operator, n2) => {
+        let result = "";
+
+        if (operator === "add") {
+          result = parseFloat(n1) + parseFloat(n2);
+        } else if (operator === "subtract") {
+          result = parseFloat(n1) - parseFloat(n2);
+        } else if (operator === "multiply") {
+          result = parseFloat(n1) * parseFloat(n2);
+        } else if (operator === "divide") {
+          result = parseFloat(n1) / parseFloat(n2);
+        }
+
+        return result;
+      };
     }
   }
 });
+
+const display = document.querySelector(".calculator__display");
